64022ed4e451f353dfb76c38e4cc76067fd055d6
Muhammed Shifil K <muhdshifilk@gmail.com>
2024-12-16 15:14:23 +0530

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server')
+
+const mpServer = jsonServer.create()
+
+//initializing middleware
+const middleWare = jsonServer.defaults()
+
+//creating router instance
+const router = jsonServer.router('db.json')
+
+//configuring middleware and routes to server
+mpServer.use(middleWare)
+mpServer.use(router)
+
+//setting port
+
+const PORT = 3000 || process.env.PORT
+
+//calling listen method with server for activating server
+mpServer.listen(PORT,()=>{
+    console.log("Server running at PORT:"+PORT);
+    
+})
